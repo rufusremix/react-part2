e935c40efe4703dd90851a5667a93b5957edb4da
Rufus <rpr@gmail.com>
2024-03-05 00:09:38 +0530

    Using React Query DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
+import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools/build/lib/devtools";
 
 const queryCLient = new QueryClient();
 
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryCLient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
