2c1b1ddb3ffde577db4d009f5c7d7105a20771ab
RufusC <rprc@gmail.com>
2024-03-06 11:55:01 +0530

    parameterized queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -2,22 +2,26 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
-  }
-const usePosts = () => {
-    const fetchPosts = () => 
-        axios
-          .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
-          .then((res) => (res.data));
-
-    return useQuery<Post[], Error>({
-        queryKey: ["posts"],
-        queryFn: fetchPosts,
-        staleTime: 1*60*1000  // 1m
-    });
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
 }
+const usePosts = (userId: number | undefined) => {
+  const fetchPosts = () =>
+    axios
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          userId,
+        },
+      })
+      .then((res) => res.data);
+
+  return useQuery<Post[], Error>({
+    queryKey: userId ? ["user", userId, "posts"] : ["posts"],
+    queryFn: fetchPosts,
+    staleTime: 1 * 60 * 1000, // 1m
+  });
+};
 
 export default usePosts;
