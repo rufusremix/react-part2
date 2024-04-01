336d8ec0f1e20ba11dc0a122f3d5966007edd24a
RufusC <rprc@gmail.com>
2024-04-01 10:46:23 +0530

    Mutating data

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,17 +1,47 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
 
 const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null);
-
+  const queryClient = useQueryClient();
+  const addTodo = useMutation<Todo, Error, Todo>({
+    mutationFn: (todo: Todo) =>
+      axios.post<Todo>("https://jsonplaceholder.typicode.com/todos", todo).then((res) => res.data),
+    onSuccess: (savedTodo, newTodo) => {
+      // Since jsonplaceholder api is fake
+      // Updating the data in cache so as newly added todo is displayed
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [savedTodo, ...(todos || [])]);
+    },
+  });
   return (
-    <form className="row mb-3">
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {addTodo.error && <div className="alert alert-danger">{addTodo.error.message}</div>}
+      <form
+        className="row mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (ref.current && ref.current.value) {
+            addTodo.mutate({
+              id: 0,
+              title: ref.current.value,
+              userId: 1,
+              completed: false,
+            });
+          }
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button disabled={addTodo.isLoading} className="btn btn-primary">
+            {addTodo.isLoading ? "Adding... " : "Add"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,23 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-interface Todo {
-    id: number;
-    title: string;
-    userId: number;
-    completed: boolean;
+export interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
 }
 
 const useTodos = () => {
-    const fetchtodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
+  const fetchtodos = () =>
+    axios.get<Todo[]>("https://jsonplaceholder.typicode.com/todos").then((res) => res.data);
 
-      return useQuery<Todo[], Error>({
-        queryKey: ["todos"],
-        queryFn: fetchtodos,
-      });
-}
+  return useQuery<Todo[], Error>({
+    queryKey: ["todos"],
+    queryFn: fetchtodos,
+  });
+};
 
 export default useTodos;
